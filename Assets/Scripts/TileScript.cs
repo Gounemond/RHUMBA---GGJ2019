2a94b33ee67cd3b8d800f89d143f4ccf44906bda
Marco <farina.marco94@gmail.com>
2019-01-26 13:39:42 +0100

    Tiles

---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour
+{
+    public int oldColour;
+    public int newColour;
+    public bool enter = true;
+    public string playerTag = "Player0";
+
+    public TilesManager tilesManagerScript;
+
+
+    void Start()
+    {      
+        oldColour = 0; //grigio iniziale
+    }
+
+    void Update(){}
+
+    private void OnTriggerEnter(Collider playerCollider)
+    {
+        Debug.Log("triggrred");
+        playerTag = playerCollider.tag;
+        switch (playerTag)
+        {
+            case "Player0":
+                newColour = 0;
+                break;
+            case "Player1":
+                newColour = 1;
+                break;
+            case "Player2":
+                newColour = 2;
+                break;
+            case "Player3":
+                newColour = 3;
+                break;
+            case "Player4":
+                newColour = 4;
+                break;
+        }
+        if (newColour!= oldColour)
+            tilesManagerScript.UpdateTilesCOunt(oldColour, newColour);
+
+        oldColour = newColour;
+    }
+
+}
---
+++ Assets/Scripts/TilesManager.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TilesManager : MonoBehaviour
+{
+    public int player0Tiles;
+    public int player1Tiles;
+    public int player2Tiles;
+    public int player3Tiles;
+    public int player4Tiles;
+
+    void Start()
+    {
+        player0Tiles = GameObject.FindGameObjectsWithTag("Tile").Length;
+        player1Tiles = 0;
+        player2Tiles = 0;
+        player3Tiles = 0;
+        player4Tiles = 0;
+    }
+
+    void Update()  {}
+
+    public void UpdateTilesCOunt(int oldcolour, int newcolour)
+    {
+        switch (oldcolour)
+        {
+            case 0:
+                player0Tiles--;
+                break;
+            case 1:
+                player1Tiles--;
+                break;
+
+            case 2:
+                player2Tiles--;
+                break;
+
+            case 3:
+                player3Tiles--;
+                break;
+
+            case 4:
+                player4Tiles--;
+                break;
+        }
+
+        switch (newcolour)
+        {
+            case 0:
+                player0Tiles++;
+                break;
+
+            case 1:
+                player1Tiles++;
+                break;
+
+            case 2:
+                player2Tiles++;
+                break;
+
+            case 3:
+                player3Tiles++;
+                break;
+
+            case 4:
+                player4Tiles++;
+                break;
+        }
+    }
+}
