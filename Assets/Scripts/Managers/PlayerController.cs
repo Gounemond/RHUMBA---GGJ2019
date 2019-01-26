4a0a53c6139e1b0aa06b52cb16e30e9c30953f9a
guitax <19864161+guitax@users.noreply.github.com>
2019-01-26 15:06:05 +0100

    PlayerController (SandardMove)

--- Assets/Scripts/Managers/PlayerController.cs
+++ Assets/Scripts/Managers/PlayerController.cs
@@ -1,38 +1,22 @@
 ﻿using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
-    public float currentMoveSpeed;
-    public float currentTurnSpeed;
-
-    void Awake() {
-    }
-
-    // Start is called before the first frame update
-    void Start() {
-        RoombaConfig roombaConfig = GameManager.Instance.gameConfig.roombaConfig;
-        currentMoveSpeed = roombaConfig.baseMoveSpeed;
-        currentTurnSpeed = roombaConfig.baseTurnSpeed;
-    }
-
-    // Update is called once per frame
     void Update() {
-        //TODO: input alternativo "Crash"
-        if(GameManager.Instance.gameConfig.roombaConfig.inputMode == RoombaInputMode.Move) {
-            var translation = GameManager.Instance.gameConfig.roombaConfig.baseMoveSpeed * Time.deltaTime;
-            var rotation = GameManager.Instance.gameConfig.roombaConfig.baseTurnSpeed * Time.deltaTime;
+        switch(GameManager.Instance.gameConfig.roombaConfig.inputMode) {
+            case RoombaInputMode.Move:
+                float translation = GameManager.Instance.gameConfig.roombaConfig.baseMoveSpeed * Time.deltaTime;
+                float rotation = GameManager.Instance.gameConfig.roombaConfig.baseTurnSpeed * Time.deltaTime;
 
-            if(Input.GetKey(KeyCode.UpArrow)) {
-                transform.Translate(Vector3.forward * translation);
-            }
-            if(Input.GetKey(KeyCode.DownArrow)) {
-                transform.Translate(-Vector3.forward * translation);
-            }
-            if(Input.GetKey(KeyCode.LeftArrow)) {
-                transform.Rotate(Vector3.up, -rotation);
-            }
-            if(Input.GetKey(KeyCode.RightArrow)) {
-                transform.Rotate(Vector3.up, rotation);
-            }
+                if(Mathf.Abs(Input.GetAxis("Horizontal")) > 0) {
+                    transform.Rotate(Vector3.up * Input.GetAxis("Horizontal") * rotation);
+                }
+                if(Mathf.Abs(Input.GetAxis("Vertical")) > 0) {
+                    transform.Translate(Vector3.forward * Input.GetAxis("Vertical") * translation);
+                }
+                break;
+            case RoombaInputMode.Crash:
+                //TODO: input alternativo "Crash"
+                break;
         }
     }
 }
