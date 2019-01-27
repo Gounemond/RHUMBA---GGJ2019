740327be79a94e0eb797c2c5d6ab9dd45c756181
guitax <19864161+guitax@users.noreply.github.com>
2019-01-27 16:04:09 +0100

    Score

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -13,13 +13,14 @@ public class ScoreManager : MonoBehaviour {
     
     IEnumerator Start() {
         yield return StartCoroutine(menuUIManager.FadeIn(1));
-        for(int i = 0; i < GameData.playerData?.Count; i++) {
-            scoreBars[i].GetComponent<Image>().color = GameData.playerData[i].graphics.trailColor;
-            RectTransform rectTransform = scoreBars[i].GetComponent<RectTransform>();
-            scoreBars[i].transform.parent.gameObject.SetActive(true);
-            float tweenDuration = scoreTweenDurationMax * GameData.playerData[i].tilesCleanedPercentage;
-            rectTransform.DOSizeDelta(new Vector2(rectTransform.rect.width, scoreBarHeightMax * GameData.playerData[i].tilesCleanedPercentage), tweenDuration);
-            StartCoroutine(scoreBars[i].GetComponentInChildren<ScoreLabelController>().CountTo(Mathf.RoundToInt(GameData.playerData[i].tilesCleanedPercentage * 10000) / 100f, tweenDuration));
+        foreach(PlayerData playerData in GameData.playerData) {
+            GameObject scoreBar = scoreBars[playerData.playerId];
+            scoreBar.GetComponent<Image>().color = playerData.graphics.trailColor;
+            scoreBar.transform.parent.gameObject.SetActive(true);
+            float tweenDuration = scoreTweenDurationMax * playerData.tilesCleanedPercentage;
+            RectTransform rectTransform = scoreBar.GetComponent<RectTransform>();
+            rectTransform.DOSizeDelta(new Vector2(rectTransform.rect.width, scoreBarHeightMax * playerData.tilesCleanedPercentage), tweenDuration);
+            StartCoroutine(scoreBar.GetComponentInChildren<ScoreLabelController>().CountTo(Mathf.RoundToInt(playerData.tilesCleanedPercentage * 10000) / 100f, tweenDuration));
         }
         yield return new WaitForSeconds(10f);
         yield return StartCoroutine(menuUIManager.FadeOut(1));
